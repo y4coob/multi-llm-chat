501de08bb77071118136e4c3b1b8842afe16243b
yacoobmohoboob <ge87hoh@mytum.de>
2025-01-28 00:30:10 +0100

    aölkjdf

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 app.use(
   cors({
     origin: 
-      process.env.FRONTEND_URL     
+      "http://165.22.66.46:3000" 
     ,             
     credentials: true,
   }),
