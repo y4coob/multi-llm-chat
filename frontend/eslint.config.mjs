60fc97a09621e6ce9cd7b832641270679ba12ce0
yacoobmohoboob <ge87hoh@mytum.de>
2025-01-27 17:41:10 +0100

    updated eslint.config.mjs to disable some rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -24,7 +24,14 @@ const eslintConfig = [
     rules: {
       ...reactPlugin.configs.recommended.rules,
       ...reactHooksPlugin.configs.recommended.rules,
+      // Disable all rules
       "react/react-in-jsx-scope": "off", // Next.js doesn't require React to be in scope
+      "react/no-unescaped-entities": "off", // Allow unescaped entities
+      "no-undef": "off", // Allow undefined variables
+      "no-unused-vars": "off", // Allow unused variables
+      "react/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off", // Disable exhaustive deps check for hooks
+      "no-console": "off", // Allow console logs
     },
     settings: {
       react: {
@@ -35,4 +42,3 @@ const eslintConfig = [
 ];
 
 export default eslintConfig;
-
