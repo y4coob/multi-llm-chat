e9c614793dc0fd744dc029f26a6e0e3f65ed34dc
yacoobmohoboob <ge87hoh@mytum.de>
2025-01-27 18:31:32 +0100

    sth with cors fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors({ origin: 'http://165.22.66.46' })); 
+app.use(cors({ origin: 'http://165.22.66.46/' })); 
 
 app.use(express.json())
 
